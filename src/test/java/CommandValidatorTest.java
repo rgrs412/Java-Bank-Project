f831bc85ebc26fdf81563d85a68bc4e9f23ebe61
rgrs412 <yuwuheng0@gmail.com>
2020-10-27 23:33:09 -0400

    refactor Test: create_command_has_less_than_4_arguments

--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -5,17 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommandValidatorTest {
 
+    CommandValidator cmdValidator;
+    String command;
+
+    @BeforeEach
+    void setUp() {
+        cmdValidator = new CommandValidator();
+    }
+
     @Test
     void create_command_has_4_arguments() {
-        String command = "Create checking 12345678 0.01";
-        CommandValidator cmdValidator = new CommandValidator();
+        command = "Create checking 12345678 0.01";
         assertTrue(cmdValidator.isValidNumberOfCommandArguments(command));
     }
 
     @Test
     void create_command_has_less_than_4_arguments() {
-        String command = "Create checking 12345678";
-        CommandValidator cmdValidator = new CommandValidator();
+        command = "Create checking 12345678";
         assertFalse(cmdValidator.isValidNumberOfCommandArguments(command));
     }
 }
