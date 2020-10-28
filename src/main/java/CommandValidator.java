dee6b2bf57a196986adc9409bd06661a9c1b6e44
rgrs412 <yuwuheng0@gmail.com>
2020-10-27 23:13:58 -0400

    refactor Test: create_command_has_4_arguments

--- src/main/java/CommandValidator.java
+++ src/main/java/CommandValidator.java
@@ -1,6 +1,6 @@
 public class CommandValidator {
 
     public boolean isValidNumberOfCommandArguments(String command) {
-        return true;
+        return command.split(" ").length == 4;
     }
 }
