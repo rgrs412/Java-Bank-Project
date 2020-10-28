1a163ebe695cb762c949025bd75b2178fb83a546
rgrs412 <yuwuheng0@gmail.com>
2020-10-27 23:21:46 -0400

    fail/pass Test: create_command_has_less_than_4_arguments

--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommandValidatorTest {
@@ -9,5 +10,12 @@ public class CommandValidatorTest {
         String command = "Create checking 12345678 0.01";
         CommandValidator cmdValidator = new CommandValidator();
         assertTrue(cmdValidator.isValidNumberOfCommandArguments(command));
+    }
+
+    @Test
+    void create_command_has_less_than_4_arguments() {
+        String command = "Create checking 12345678";
+        CommandValidator cmdValidator = new CommandValidator();
+        assertFalse(cmdValidator.isValidNumberOfCommandArguments(command));
     }
 }
