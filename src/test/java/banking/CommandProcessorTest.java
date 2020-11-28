c9a845183e72ee1c9aa309032cfd41225d36d2ca
rgrs412 <yuwuheng0@gmail.com>
2020-11-28 10:37:05 -0500

    Invalid command type is not processed

--- src/test/java/banking/CommandProcessorTest.java
+++ src/test/java/banking/CommandProcessorTest.java
@@ -1,9 +1,14 @@
 package banking;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandProcessorTest {
 
+    public static final String INVALID_CREATE_CHECKING_COMMAND = "invalid_create checking 12345678 0.01";
+    public static final String ID = "12345678";
     Bank bank;
     CommandProcessor commandProcessor;
 
@@ -13,4 +18,9 @@ public class CommandProcessorTest {
         commandProcessor = new CommandProcessor(bank);
     }
 
+    @Test
+    void invalid_command_type_is_not_processed() {
+        commandProcessor.processCommand(INVALID_CREATE_CHECKING_COMMAND);
+        assertEquals(null, bank.getBankAccounts().get(ID));
+    }
 }
