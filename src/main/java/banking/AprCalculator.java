fd525340f96f4f720f2a79ae79b60970a91dfca0
rgrs412 <yuwuheng0@gmail.com>
2020-11-29 13:32:54 -0500

    cd account apr is calculated correctly

--- src/main/java/banking/AprCalculator.java
+++ src/main/java/banking/AprCalculator.java
@@ -4,6 +4,16 @@ public class AprCalculator {
 
     public double calculateInterest(BankAccount bankAccount) {
         double decimalApr = bankAccount.getApr() / 100;
-        return (decimalApr / 12) * bankAccount.getBalance();
+        if (bankAccount.getAccountType().equals("cd")) {
+            double currentBalance = bankAccount.getBalance();
+            double currentAccruedInterest;
+            for (int i = 0; i < 4; i++) {
+                currentAccruedInterest = (decimalApr / 12) * currentBalance;
+                currentBalance += currentAccruedInterest;
+            }
+            return currentBalance - bankAccount.getBalance();
+        } else {
+            return (decimalApr / 12) * bankAccount.getBalance();
+        }
     }
 }
